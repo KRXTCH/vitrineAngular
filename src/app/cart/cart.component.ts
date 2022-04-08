e9e615bb71c37056d59501eb66a596867f221c6a
Lorys Jelezian <lorysjele@gmail.com>
2022-04-08 12:11:12 +0200

    adapt ProductCart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,14 +11,16 @@ import {ProductCart} from "../models/ProductCart";
 export class CartComponent implements OnInit {
 
   constructor(private cartService: CartService) {
-    this.cartService.addToCart(new ProductCart(new Product(9,
+    this.cartService.addToCart(new ProductCart(
+      9,
       "Horizon Zero Dawn",
       "Horizon Zero Dawn for PC is an action role playing game in which you control Aloy, a rare human survivor in the 31st century dystopian landscape. Humans may be rare, and access to technology is limited, but there is a large population of unfriendly robotic creatures (who were originally created by Big Tech who then lost control of them).",
       "https://s2.gaming-cdn.com/images/products/6202/271x377/horizon-zero-dawn-complete-edition-pc-game-steam-cover.jpg",
       17.49,
       new Date("05/27/2020"),
-      "https://s2.gaming-cdn.com/img/products/6202/pcover/6202.jpg"
-    ), 1));
+      "https://s2.gaming-cdn.com/img/products/6202/pcover/6202.jpg",
+      "Standard",
+      1));
   }
 
   ngOnInit(): void {
